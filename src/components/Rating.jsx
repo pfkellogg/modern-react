a4bc563fdfdb7511458c40f51faee6cb970b902a
Patrick <pfkellogg@gmail.com>
2025-11-07 17:21:36 -0500

    lists

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,7 +1,17 @@
 const Rating = () => {
+  // The Array.from() method returns an array from any object with a length property.
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1)
+
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
+      <div className="stars">
+        {stars.map(star => (
+          <span key={star} className="star">
+            {'\u2605'}
+          </span>
+        ))}
+      </div>
     </div>
   )
 }
