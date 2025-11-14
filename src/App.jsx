94ac520909a8f6617ee19666d8af682a11eff2ad
Patrick <pfkellogg@gmail.com>
2025-11-14 11:31:20 -0500

    more on useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import Rating from './components/Rating'
+import { useState } from 'react'
+
+const Counter = () => {
+  const [count, setCount] = useState(() => {
+    console.log('init count state ...')
+    return 0
+  })
+
+  const increment = () => {
+    setCount(prevCount => {
+      console.log(prevCount)
+      return prevCount + 1
+    })
+  }
+
+  return (
+    <>
+      <h2>Count: {count}</h2>
+      <button onClick={increment}>Increment</button>
+    </>
+  )
+}
 
 const App = () => {
   return (
     <div>
       <Rating />
+      <Counter />
     </div>
   )
 }
