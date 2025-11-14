65ac30bfe9f2e5acf19b9fa69eda1d9b169160bc
Patrick <pfkellogg@gmail.com>
2025-11-14 12:45:22 -0500

    component props

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
 import Rating from './components/Rating'
 import { useState } from 'react'
 
-const Counter = () => {
-  const [count, setCount] = useState(() => {
-    console.log('init count state ...')
-    return 0
-  })
+// const Counter = () => {
+//   const [count, setCount] = useState(() => {
+//     console.log('init count state ...')
+//     return 0
+//   })
 
-  const increment = () => {
-    setCount(prevCount => {
-      console.log(prevCount)
-      return prevCount + 1
-    })
-  }
+//   const increment = () => {
+//     setCount(prevCount => {
+//       console.log(prevCount)
+//       return prevCount + 1
+//     })
+//   }
 
-  return (
-    <>
-      <h2>Count: {count}</h2>
-      <button onClick={increment}>Increment</button>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <h2>Count: {count}</h2>
+//       <button onClick={increment}>Increment</button>
+//     </>
+//   )
+//}
 
 const App = () => {
   return (
     <div>
-      <Rating />
-      <Counter />
+      <Rating
+        heading="How do you feel about React?"
+        feedbackMessages={[
+          'Hate it',
+          'Dislike it',
+          'So so',
+          'Like it',
+          'Love it'
+        ]}
+      />
+
+      {/* <Counter /> */}
     </div>
   )
 }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 
-const Rating = () => {
+const Rating = ({
+  heading = 'Rate Your Experience',
+  color = 'gold',
+  feedbackMessages = ['Terrible', 'Poor', 'Fair', 'Good', 'Excellent']
+}) => {
   const [rating, setRating] = useState(0)
   const [hover, setHover] = useState(0)
 
   // The Array.from() method returns an array from any object with a length property.
   const stars = Array.from({ length: 5 }, (_, i) => i + 1)
-  const feedbackMessages = ['Terrible', 'Poor', 'Average', 'Good', 'Excellent']
 
+  // console.log(heading)
   return (
     <div className="rating-container">
-      <h2>Rate Your Experience</h2>
+      <h2>{heading}</h2>
       <div className="stars">
         {stars.map(star => (
           <span
@@ -18,7 +22,10 @@ const Rating = () => {
             onMouseLeave={() => setHover(0)}
             onClick={() => setRating(star)}
             key={star}
-            className={`star ${star <= (hover || rating) ? 'active' : ''}`}
+            className="star"
+            style={{
+              color: star <= (hover || rating) ? color : '#ccc'
+            }}
           >
             {'\u2605'}
           </span>
